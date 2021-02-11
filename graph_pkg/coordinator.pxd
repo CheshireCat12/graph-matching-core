9d76959f499ef4d572901030564eccfba92503da
anthony gillioz <anthonygillioz@schoenfix.inf.unibe.ch>
2021-02-11 18:04:09 +0100

    add the coordinator class

---
+++ graph_pkg/coordinator.pxd
@@ -0,0 +1,29 @@
+from graph_pkg.edit_cost.edit_cost cimport EditCost
+from graph_pkg.edit_cost.edit_cost_letter cimport EditCostLetter
+from graph_pkg.edit_cost.edit_cost_AIDS cimport EditCostAIDS
+from graph_pkg.edit_cost.edit_cost_mutagenicity import EditCostMutagenicity
+from graph_pkg.algorithm.graph_edit_distance cimport GED
+from graph_pkg.loader.loader_base import LoaderBase
+from graph_pkg.loader.loader_letter import LoaderLetter
+from graph_pkg.loader.loader_AIDS import LoaderAIDS
+from graph_pkg.loader.loader_mutagenicity import LoaderMutagenicity
+
+cdef class Coordinator:
+
+    cdef:
+        readonly str dataset
+        readonly tuple params_edit_cost
+        readonly list graphs
+        readonly list dataset_available
+
+        EditCost edit_cost
+        readonly GED ged
+        object loader
+
+    cdef int _init_system(self) except? -1
+
+    cdef void _init_letter(self)
+
+    cdef void _init_AIDS(self)
+
+    cdef void _init_mutagenicity(self)
---
+++ graph_pkg/coordinator.pyx
@@ -0,0 +1,35 @@
+cdef class Coordinator:
+
+    def __cinit__(self, str dataset, tuple params_edit_cost):
+        self.dataset = dataset
+        self.params_edit_cost = params_edit_cost
+        self._init_system()
+
+    cdef int _init_system(self) except? -1:
+        """
+        
+        :return: 
+        """
+        if self.dataset == 'letter':
+            self._init_letter()
+        elif self.dataset == 'AIDS':
+            self._init_AIDS()
+        elif self.dataset == 'mutagenicity':
+            self._init_mutagenicity()
+        else:
+            raise ValueError(f'The dataset {self.dataset} is not available')
+
+        self.graphs = self.loader.load()
+        self.ged = GED(self.edit_cost)
+
+    cdef void _init_letter(self):
+        self.loader = LoaderLetter('HIGH')
+        self.edit_cost = EditCostLetter(*self.params_edit_cost)
+
+    cdef void _init_AIDS(self):
+        self.loader = LoaderAIDS()
+        self.edit_cost = EditCostAIDS(*self.params_edit_cost)
+
+    cdef void _init_mutagenicity(self):
+        self.loader = LoaderMutagenicity()
+        self.edit_cost = EditCostMutagenicity(*self.params_edit_cost)
