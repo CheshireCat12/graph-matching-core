f368b9b4879c24f8d4203bf5f9d51318a4edccba
mfuchs <mathias.fuchs@inf.unibe.ch>
2021-03-10 14:43:22 +0100

    add variable definition

--- graph_pkg/algorithm/levenshtein_distance.pyx
+++ graph_pkg/algorithm/levenshtein_distance.pyx
@@ -9,9 +9,15 @@ cdef double compute_edit_distance(str string_1, str string_2, double subst_cost,
     return _compute_edit_distance_matr(string_1, string_2, subst_cost, ins_cost, del_cost)[len_1][len_2]
 
 cpdef double[:, ::1] _compute_edit_distance_matr_cpd(str string_1, str string_2, double subst_cost, double ins_cost, double del_cost):
+
     return _compute_edit_distance_matr(string_1, string_2, subst_cost, ins_cost, del_cost)
 
 cdef double[:, ::1] _compute_edit_distance_matr(str string_1, str string_2, double subst_cost, double ins_cost, double del_cost):
+    cdef:
+        int len_1, len_2
+        list str_1_list, str_2_list
+        double[:, ::1] distances
+
     len_1 = len(string_1)
     len_2 = len(string_2)
     str_1_list = list(string_1)
