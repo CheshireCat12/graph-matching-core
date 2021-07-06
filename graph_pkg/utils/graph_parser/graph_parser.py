870fd656fa48c0aa25c10aa1eed2c1d966228290
CheshireCat12 <anthony.gillioz@outlook.com>
2021-07-06 12:28:11 +0200

    change the graph parser to work with more dataset

--- graph_pkg/utils/graph_parser/graph_parser.py
+++ graph_pkg/utils/graph_parser/graph_parser.py
@@ -13,39 +13,69 @@ import xml.etree.ElementTree as ET
 import xml.dom.minidom as md
 import random
 
+# Choose which dataset to parse
+__CHOSEN_DATASET = 1
 
-__FOLDER = './data/NCI1/'
-__DATASET = 'NCI1'
+##################
+# Init Constants #
+##################
+__DATASETS = ['NCI1', 'PROTEINS', 'ENZYMES']
+__DATASET_NAME = __DATASETS[__CHOSEN_DATASET]
+__FOLDER = f'./data/{__DATASET_NAME}/'
 __EXTENSIONS = {
-    'edges': '.edges',
-    'graph_idx': '.graph_idx',
-    'graph_labels': '.graph_labels',
-    'node_labels': '.node_labels',
-}
+        'edges': f'{__DATASET_NAME}_A.txt',
+        'graph_idx': f'{__DATASET_NAME}_graph_indicator.txt',
+        'graph_labels': f'{__DATASET_NAME}_graph_labels.txt',
+        'node_labels': f'{__DATASET_NAME}_node_labels.txt',
+    }
+# upper limit for train and val
+# Take into account the number of classes
+__SPLIT_CLASSES = {'NCI1': (750, 250),
+                   'PROTEINS': (330, 110),
+                   # 'ENZYMES': (180, )
+                   }
+
 
 
 def parser(folder, dataset):
-    # nodes_lbls_per_graph = load_nodes_per_graph_with_lbls(folder, dataset)
-    # edges = load_edges(folder, dataset)
-    # edges_per_graph = create_edges_per_graph(nodes_lbls_per_graph, edges)
-    #
-    # parse_graph_xml(nodes_lbls_per_graph, edges_per_graph, folder)
+    # Create the graphs with the nodes and edges
+    nodes_lbls_per_graph = load_nodes_per_graph_with_lbls(folder)
+    edges = load_edges(folder)
+    edges_per_graph = create_edges_per_graph(nodes_lbls_per_graph, edges)
 
-    graph_lbls = load_file(folder, dataset, __EXTENSIONS['graph_labels'])
+    parse_graph_xml(nodes_lbls_per_graph, edges_per_graph, folder)
+
+    # Create the splitting between the tr, va, te
+    graph_lbls = load_file(folder, __EXTENSIONS['graph_labels'])
     labels_per_graph = defaultdict(list)
+
+    classes = set()
+
     for idx, graph_lbl in enumerate(graph_lbls):
+        classes.add(int(graph_lbl))
         labels_per_graph[int(graph_lbl)].append((idx, int(graph_lbl)))
 
-    random.seed(42)
-    random.shuffle(labels_per_graph[0])
-    random.shuffle(labels_per_graph[1])
+    split_tr, split_va = __SPLIT_CLASSES[dataset]
 
-    graphs_train = labels_per_graph[0][:750] + labels_per_graph[1][:750]
-    graphs_val = labels_per_graph[0][750:1000] + labels_per_graph[1][750:1000]
-    graphs_test = labels_per_graph[0][1000:] + labels_per_graph[1][1000:]
-    print(len(labels_per_graph[0]))
-    print(len(labels_per_graph[1]))
+    random.seed(42)
+    for class_ in sorted(classes):
+        random.shuffle(labels_per_graph[class_])
+
+
+    graphs_train, graphs_val, graphs_test = [], [], []
+
+    for class_ in sorted(classes):
+        graphs_train += labels_per_graph[class_][:split_tr]
+        graphs_val += labels_per_graph[class_][split_tr:split_tr+split_va]
+        graphs_test += labels_per_graph[class_][split_tr+split_va:]
+
+    # graphs_train = labels_per_graph[0][:750] + labels_per_graph[1][:750]
+    # graphs_val = labels_per_graph[0][750:1000] + labels_per_graph[1][750:1000]
+    # graphs_test = labels_per_graph[0][1000:] + labels_per_graph[1][1000:]
+    # print(len(labels_per_graph[0]))
+    # print(len(labels_per_graph[1]))
     print(len(graphs_train))
+    print(len(graphs_val))
     print(len(graphs_test))
     parse_class_xml(graphs_train, folder, 'train')
     parse_class_xml(graphs_val, folder, 'validation')
@@ -133,8 +163,8 @@ def create_edges_per_graph(nodes_lbls_per_graph, edges):
     return edges_per_graph
 
 
-def load_edges(folder, dataset):
-    raw_edges = load_file(folder, dataset, __EXTENSIONS['edges'])
+def load_edges(folder):
+    raw_edges = load_file(folder, __EXTENSIONS['edges'])
 
     edges = []
     Edge = namedtuple('Edge', ['node_idx_start', 'node_idx_end'])
@@ -147,9 +177,9 @@ def load_edges(folder, dataset):
     return edges
 
 
-def load_nodes_per_graph_with_lbls(folder, dataset):
-    graph_idx = load_file(folder, dataset, __EXTENSIONS['graph_idx'])
-    node_labels = load_file(folder, dataset, __EXTENSIONS['node_labels'])
+def load_nodes_per_graph_with_lbls(folder):
+    graph_idx = load_file(folder, __EXTENSIONS['graph_idx'])
+    node_labels = load_file(folder, __EXTENSIONS['node_labels'])
 
     nodes_per_graph = defaultdict(list)
     Node = namedtuple('Node', ['node_id', 'node_lbl'])
@@ -161,15 +191,15 @@ def load_nodes_per_graph_with_lbls(folder, dataset):
     return nodes_per_graph
 
 
-def load_file(folder, dataset, extension):
-    filename = os.path.join(folder, f'{dataset}{extension}')
+def load_file(folder, extension):
+    filename = os.path.join(folder, extension)
     with open(filename, mode='r') as fp:
         data = fp.readlines()
     return data
 
 
 def main():
-    parser(__FOLDER, __DATASET)
+    parser(__FOLDER, __DATASET_NAME)
 
 
 if __name__ == '__main__':
